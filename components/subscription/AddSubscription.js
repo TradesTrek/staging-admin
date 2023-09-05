32a21709be3715eede10e01ecde3ee4c610eda51
balogun akeem <balogunbiola101@gmail.com>
2023-09-05 15:54:45 +0100

    fix subscription on admin

--- components/subscription/AddSubscription.js
+++ components/subscription/AddSubscription.js
@@ -11,19 +11,18 @@ export default function AddSubscription({ addUserCloseModal }) {
   const baseUrl = `${publicRuntimeConfig.apiUrl}`;
   const toastId = useRef(null);
   const intialValues = {
-          packageName: "",
+    packageName: "",
     packageDuration: "monthly",
-    packageAmount:0,
-    packageDesc:""
- 
+    packageAmount: 0,
+    packageDesc: "",
+    category: "Premium",
   };
   const [formValues, setFormValues] = useState(intialValues);
   const [formErrors, setFormErrors] = useState({});
   const [updateLoader, setUpdateLoader] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
- 
+
   useEffect(() => {
-    console.log(formErrors);
     if (Object.keys(formErrors).length === 0 && isSubmitting) {
       submitEditForm();
     }
@@ -39,13 +38,12 @@ export default function AddSubscription({ addUserCloseModal }) {
   const submitEditForm = async () => {
     setUpdateLoader(true);
     const data = {
-          packageName: formValues?.packageName,
+      packageName: formValues?.packageName,
       packageDuration: formValues?.packageDuration,
-      packageAmount:formValues?.packageAmount,
-      packageDesc:formValues?.packageDesc
-     
+      packageAmount: formValues?.packageAmount,
+      packageDesc: formValues?.packageDesc,
+      category: formValues?.category
     };
-
 
     const response = await subscriptionService.AddSubscription(data);
     if (response.success) {
@@ -82,24 +80,28 @@ export default function AddSubscription({ addUserCloseModal }) {
     let errors = {};
     if (!values.packageName) {
       errors.packageName = "Package name is required.";
-    }else if(values.packageName.length>50){
+    } else if (values.packageName.length > 50) {
       errors.packageName = "Package name is maximum 50 characters.";
     }
     if (!values.packageDuration) {
       errors.packageDuration = "Package duration is required";
     }
+    if (!values.category) {
+      errors.category = "Package category is required";
+    }
     if (!values.packageAmount) {
       errors.packageAmount = "Package amount is required";
     }
-    if (values.packageAmount<100) {
-      errors.packageAmount = 'Package Amount must be greater then or equal to 100';
+    if (values.packageAmount < 100) {
+      errors.packageAmount =
+        "Package Amount must be greater then or equal to 100";
     }
     if (!values.packageDesc) {
       errors.packageDesc = "Package description is required";
-    }else if(values.packageDesc.length>80){
-      errors.packageDesc="Package description is maximum 80 characters."
+    } else if (values.packageDesc.length > 80) {
+      errors.packageDesc = "Package description is maximum 80 characters.";
     }
-   
+
     return errors;
   };
 
@@ -122,7 +124,6 @@ export default function AddSubscription({ addUserCloseModal }) {
         </div>
         <div className="user--form">
           <form id="addLocks" onSubmit={handleSubmit} noValidate>
-           
             <div className="form--item">
               <label className="form--label" htmlFor="lockModel">
                 Package Name*
@@ -140,7 +141,7 @@ export default function AddSubscription({ addUserCloseModal }) {
             </div>
             <div className="form--item">
               <label className="form--label" htmlFor="brand">
-              Package Duration*
+                Package Duration*
               </label>
               <select
                 className={`form--control ${
@@ -150,44 +151,96 @@ export default function AddSubscription({ addUserCloseModal }) {
                 value={formValues.packageDuration}
                 onChange={handleChange}
               >
-                  {/* <option
+                {/* <option
                   value='trial'
                   selected={formValues.packageDuration == 'trial' ? true : false}
                 >
                 Free  Trial
                 </option> */}
                 <option
-                  value='hourly'
-                  selected={formValues.packageDuration == 'hourly' ? true : false}
+                  value="hourly"
+                  selected={
+                    formValues.packageDuration == "hourly" ? true : false
+                  }
                 >
                   Hourly
                 </option>
                 <option
-                  value='monthly'
-                  selected={formValues.packageDuration == 'monthly' ? true : false}
+                  value="monthly"
+                  selected={
+                    formValues.packageDuration == "monthly" ? true : false
+                  }
                 >
                   Monthly
                 </option>
                 <option
-                  value='quarterly'
-                  selected={formValues.packageDuration == 'quarterly' ? true : false}
+                  value="quarterly"
+                  selected={
+                    formValues.packageDuration == "quarterly" ? true : false
+                  }
                 >
                   Quarterly
                 </option>
                 <option
-                  value='annually'
-                  selected={formValues.packageDuration == 'annually' ? true : false}
+                  value="annually"
+                  selected={
+                    formValues.packageDuration == "annually" ? true : false
+                  }
                 >
                   Annually
                 </option>
                 <option
-                  value='free-lifetime'
-                  selected={formValues.packageDuration == 'free-lifetime' ? true : false}
+                  value="free-lifetime"
+                  selected={
+                    formValues.packageDuration == "free-lifetime" ? true : false
+                  }
                 >
                   free-lifetime
                 </option>
               </select>
-              <div className="invalid-feedback">{formErrors?.packageDuration}</div>
+              <div className="invalid-feedback">
+                {formErrors?.packageDuration}
+              </div>
+            </div>
+
+            <div className="form--item">
+              <label className="form--label" htmlFor="brand">
+                Package Category*
+              </label>
+              <select
+                className={`form--control ${
+                  formErrors.category ? "is-invalid" : ""
+                }`}
+                name="category"
+                value={formValues.category}
+                onChange={handleChange}
+              >
+
+
+
+
+                <option
+                  value="Premium"
+                  selected={formValues.category == "Premium" ? true : false}
+                >
+                  Premium
+                </option>
+                <option
+                  value="Standard"
+                  selected={formValues.category == "Standard" ? true : false}
+                >
+                  Standard
+                </option>
+                <option
+                  value="Basic"
+                  selected={formValues.category == "Basic" ? true : false}
+                >
+                  Basic
+                </option>
+              </select>
+              <div className="invalid-feedback">
+                {formErrors?.category}
+              </div>
             </div>
             <div className="form--item">
               <label className="form--label" htmlFor="lockModel">
@@ -219,18 +272,13 @@ export default function AddSubscription({ addUserCloseModal }) {
               />
               <div className="invalid-feedback">{formErrors.packageDesc}</div>
             </div>
-           
-            
+
             <div className="form--actions">
               {updateLoader ? (
                 <FormSpinner />
               ) : (
                 <>
-                  <input
-                    type="submit"
-                    className="form--submit"
-                    value="Save"
-                  />
+                  <input type="submit" className="form--submit" value="Save" />
                 </>
               )}
             </div>
